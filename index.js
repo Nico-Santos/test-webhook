82800c34ae3ed7ddd8d1e90658c6f1135221cff2
Nico-Santos <santos.m.nicolas@gmail.com>
2023-04-20 19:04:44 -0300

    3

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 4567;
 
-app.post('http://91e5-2800-810-5a9-d83-e5d8-5ed1-7626-18c4.sa.ngrok.io/webhook',
+app.post('https://4b35-2800-810-5a9-d83-e5d8-5ed1-7626-18c4.ngrok-free.app/webhook',
     (req, res) => {
         console.log("Req: ", req);
         console.log("Req body: ", req.body);
 
         res.send("Webhook ok!!");
+});
+
+app.get('https://4b35-2800-810-5a9-d83-e5d8-5ed1-7626-18c4.ngrok-free.app/',
+    (req, res) => {
+        res.send("Simple GET");
 });
 
 app.listen(PORT, () => {
