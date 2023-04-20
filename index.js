cf1127c03a8b6a520eba927fc6160c2c1ab8c581
Nico-Santos <santos.m.nicolas@gmail.com>
2023-04-20 18:52:19 -0300

    Commit 2

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 4567;
 
-app.post('/webhook', (req, res) => {
-    console.log("Req: ", req);
-    console.log("Req body: ", req.body);
+app.post('http://91e5-2800-810-5a9-d83-e5d8-5ed1-7626-18c4.sa.ngrok.io/webhook',
+    (req, res) => {
+        console.log("Req: ", req);
+        console.log("Req body: ", req.body);
 
-    res.send("Webhook ok!!");
+        res.send("Webhook ok!!");
 });
 
 app.listen(PORT, () => {
